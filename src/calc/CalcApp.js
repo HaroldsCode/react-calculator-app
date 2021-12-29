d742046533b7407a9d41cbcec474745437f8a37c
Harold Moreno <haroldmoreno720@gmail.com>
2021-12-29 16:41:30 -0500

    inicios

---
+++ src/calc/CalcApp.js
@@ -0,0 +1,7 @@
+export const CalcApp = () => {
+  return (
+    <main>
+        <h1>Hola</h1>
+    </main>
+  )
+}
---
+++ src/calc/CalcAppStyles.js
@@ -0,0 +1,28 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  html{
+    min-height: 85vh;
+    height: 100%;
+  }
+  body, #root{
+    height: 100%;
+  }
+`;
+
+export const Themes = {
+  one: {
+    
+  },
+  two: {
+
+  },
+  three: {
+
+  }
+}
